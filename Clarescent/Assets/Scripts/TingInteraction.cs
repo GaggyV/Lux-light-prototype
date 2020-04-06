a7c703ef709a7ef06e27a7b2a4f3e697d351f90f
euleristic <valter.ottenvang@gmail.com>
2020-04-06 15:26:26 +0200

    Update TingInteraction.cs

--- Clarescent/Assets/Scripts/TingInteraction.cs
+++ Clarescent/Assets/Scripts/TingInteraction.cs
@@ -6,14 +6,36 @@ public class TingInteraction : MonoBehaviour
 {
     public bool canLevitate;
     public bool canBreak;
+    public float breakSpeed;
     public bool canBeScared;
+    public float scareSpeed;
     public Rigidbody2D body;
     public bool broken;
     public bool scared;
-    public float scareSpeed;
+
+    [SerializeField] Sprite restoredSprite, brokenSprite;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    private void Update()
+    {
+        if (broken && spriteRenderer.sprite != brokenSprite)
+            spriteRenderer.sprite = brokenSprite;
+        if (!broken && spriteRenderer.sprite != restoredSprite)
+            spriteRenderer.sprite = restoredSprite;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (body.velocity.magnitude >= breakSpeed)
+        {
+            broken = true;
+        }
+    }
+
 }
