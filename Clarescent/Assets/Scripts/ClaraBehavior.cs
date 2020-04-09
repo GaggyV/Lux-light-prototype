8b7d6bf39b6dea111d63e00c24b9999331375ed3
euleristic <valter.ottenvang@gmail.com>
2020-04-09 13:34:01 +0200

    Worked on Clara's variables

--- Clarescent/Assets/Scripts/ClaraBehavior.cs
+++ Clarescent/Assets/Scripts/ClaraBehavior.cs
@@ -4,30 +4,55 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ClaraBehavior : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float speedCap;
-    [SerializeField] private float maxJumpStrength;
-    [SerializeField] private InputHandler inputHandler;
+    [Header("Walking")]
+    [SerializeField] private float horizontalAcceleration;
+    [SerializeField] private float maximumHorizontalSpeed;
+    [SerializeField] private float horizontalFriction;
+    [SerializeField] private float drag;
+    
+    [Header("Jump")]
+    [SerializeField] private float minInputForJump;
+    [SerializeField] private float minJumpHeight;
+    [SerializeField] private float maxInputForJump;
+    [SerializeField] private float maxJumpHeight;
+    private float minJumpSqrt;
+    private float maxJumpSqrt;
+    private float gravityCoEf;
     private bool onGround;
     private Rigidbody2D rb;
     [SerializeField] private float currentVelocity;
     [SerializeField] private float deathSpeed;
     private bool dead;
+    [Header("Super secret programming magic")]
+    [SerializeField] private InputHandler inputHandler;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //rb.sharedMaterial.friction = horizontalFriction;
+        rb.drag = drag;
+        minJumpSqrt = Mathf.Sqrt(minJumpHeight);
+        maxJumpSqrt = Mathf.Sqrt(maxJumpHeight);
+        gravityCoEf = Mathf.Sqrt(Physics2D.gravity.magnitude) * Mathf.Sqrt(2f);
     }
-
     void Update()
     {
         if (dead) return;
         if (inputHandler.leftStick.x_axis != 0f || inputHandler.leftStick.y_axis != 0f)
-            rb.velocity += new Vector2(inputHandler.leftStick.x_axis * movementSpeed * Time.deltaTime, 0f);
-        if (rb.velocity.x > speedCap) rb.velocity = new Vector2(speedCap, rb.velocity.y);
-        if (rb.velocity.x < -speedCap) rb.velocity = new Vector2(-speedCap, rb.velocity.y);
-        if (inputHandler.leftTriggerAnalog.axis != -1f && onGround)
-            rb.velocity = new Vector2(rb.velocity.x, maxJumpStrength * inputHandler.leftTriggerAnalog.axis);
+            rb.velocity += new Vector2(inputHandler.leftStick.x_axis * horizontalAcceleration * Time.deltaTime, 0f);
+        if (rb.velocity.x > maximumHorizontalSpeed) rb.velocity = new Vector2(maximumHorizontalSpeed, rb.velocity.y);
+        if (rb.velocity.x < -maximumHorizontalSpeed) rb.velocity = new Vector2(-maximumHorizontalSpeed, rb.velocity.y);
+
+        if (inputHandler.leftTriggerAnalog.axis >= maxInputForJump && onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, gravityCoEf * maxJumpSqrt);
+            onGround = false;
+        }
+        else if (inputHandler.leftTriggerAnalog.axis >= minInputForJump && onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, gravityCoEf * minJumpSqrt + gravityCoEf * ((maxJumpSqrt - minJumpSqrt) / maxJumpSqrt) * ((inputHandler.leftTriggerAnalog.axis - minInputForJump) / (maxInputForJump - minInputForJump)));
+            onGround = false;
+        }
 
     }
 
