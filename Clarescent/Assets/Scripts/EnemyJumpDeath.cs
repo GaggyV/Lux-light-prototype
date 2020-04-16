ead0bed442d0a96830713e821625fe9e909d2cc4
GaggyV <60134108+GaggyV@users.noreply.github.com>
2020-04-16 14:50:32 +0200

    enemy updates

---
+++ Clarescent/Assets/Scripts/EnemyJumpDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJumpDeath : ScaredEnemy
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(clara))
+        {
+            Debug.Log("nani box");
+            enemy.gameObject.GetComponent<SpriteRenderer>().sprite = Dead;
+            enemy.transform.position = new Vector2(0, -200).normalized;
+            this.GetComponent<BoxCollider>().enabled = false;
+            Destroy(enemy.GetComponent<Rigidbody2D>());
+            Destroy(this.gameObject);
+        }
+    }
+
+}
--- Clarescent/Assets/Scripts/ScaredEnemy.cs
+++ Clarescent/Assets/Scripts/ScaredEnemy.cs
@@ -7,37 +7,58 @@ public class ScaredEnemy : MonoBehaviour
     Vector2 direction;
     [SerializeField] float detectionRange;
     [SerializeField] float speed;
+    //[SerializeField] float runAwayDistance;
     public GameObject clara;
+    public GameObject enemy;
+    public Sprite Walking;
+    public Sprite Dead;
     //public Animator animator;
     Rigidbody2D rb;
+    
 
-
-    // Start is called before the first frame update
     void Start()
     {
         //clara = GameObject.FindGameObjectWithTag("Clara");
-        rb = GetComponent<Rigidbody2D>();
-    }
+        rb = enemy.GetComponent<Rigidbody2D>();
 
-    // Update is called once per frame
+    }
     void Update()
     {
         detectionRange = (clara.transform.position - transform.position).magnitude;
         direction = (transform.position - clara.transform.position).normalized * speed;
 
-        if (detectionRange < 3)
+        if (detectionRange < 2)
         {
             rb.velocity += new Vector2(direction.x, 0);
+            enemy.gameObject.GetComponent<SpriteRenderer>().sprite = Walking;
             //animator.SetFloat("speed", Mathf.Abs(speed)); 
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+
+
+
+
+
+
+
+
+
+   /* private void OnCollisionEnter2D(Collision2D collision)
     {
-        if  (collision.gameObject.name == "Clara")
+        if (collision.gameObject.tag == "Clara")
         {
-            Destroy(this);
+            Debug.Log("Nani");
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Dead;
+            //Destroy(rb);
+            transform.position = new Vector2(0, -200).normalized;
+            Destroy(this.gameObject);
             //animator.SetBool("isdead", true);
         }
-    }
+    }*/
+
 }
+        /*
+            enemy.GetComponent<Rigidbody2D>().velocity = (new Vector2(direction.x , direction.y));
+     */
