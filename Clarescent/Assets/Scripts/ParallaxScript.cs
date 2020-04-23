5acf292922c9c5e6c06aafb6e50d151e66fb94a2
Mirriu <Irma.Andersson.0465@student.uu.se>
2020-04-23 18:48:26 +0200

    Scene with parallax

--- Clarescent/Assets/Scripts/ParallaxScript.cs
+++ Clarescent/Assets/Scripts/ParallaxScript.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxScript : MonoBehaviour {
-    private Transform cameraTransform;
-    private Vector3 lastCameraPosition;
 
-    private void Start() {
-        cameraTransform = Camera.main.transform;
-        lastCameraPosition = cameraTransform.position;
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-   private void LateUpdate() {
-        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += deltaMovement;
-        lastCameraPosition = cameraTransform.position;
+    private void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
