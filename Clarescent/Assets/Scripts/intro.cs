4fc5144ae61185fe72eb62f81427d7f6e3c7383b
GaggyV <60134108+GaggyV@users.noreply.github.com>
2020-04-27 11:21:41 +0200

    stuff

---
+++ Clarescent/Assets/Scripts/intro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class intro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Waiting());
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(27);
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
