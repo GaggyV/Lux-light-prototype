f9d17641a8d1f24b124fe23dc48611fda7f13d94
GaggyV <60134108+GaggyV@users.noreply.github.com>
2020-05-04 00:31:37 +0200

    Start n end menu

---
+++ Clarescent/Assets/Mainmenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+
+    public void StartYO()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitYO()
+    {
+        Application.Quit();
+    }
+
+    public void Restartyo()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
