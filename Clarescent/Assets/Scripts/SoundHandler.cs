411ceb6ddfcbf1448367d424f0eec3aa040a7eda
euleristic <valter.ottenvang@gmail.com>
2020-05-14 17:32:46 +0200

    Sound handler

---
+++ Clarescent/Assets/Scripts/SoundHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+    [SerializeField] SoundTrack musicTrack;
+    [SerializeField] SoundTrack walkingTrack;
+    [SerializeField] SoundTrack SFXTrack0;
+    [SerializeField] SoundTrack SFXTrack1;
+
+    public bool walking;
+    private bool walkingLagger;
+    public AudioClip walkingClip;
+    public AudioClip jumpingClip;
+    //AudioSource source;
+    void Start()
+    {
+        //source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        //if (walking != walkingLagger && walking)
+        //{
+        //    walkingLagger = walking;
+        //    source.clip = walkingClip;
+        //    source.loop = true;
+        //    source.Play();
+        //}
+        //else if (walking != walkingLagger && !walking)
+        //{
+        //    walkingLagger = walking;
+        //    source.clip = null;
+        //    source.loop = false;
+        //}
+    }
+
+    public void Jump()
+    {
+        if (SFXTrack0.free)
+        {
+            SFXTrack0.Play(jumpingClip);
+        }
+        else if (SFXTrack1.free)
+        {
+            SFXTrack1.Play(jumpingClip);
+        }
+    }
+}
