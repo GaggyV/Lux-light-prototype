15f0dba80f3e5635853345795401449e24c56d30
Mirriu <Irma.Andersson.0465@student.uu.se>
2020-05-20 17:20:20 +0200

    Cutscene 1

---
+++ Clarescent/Assets/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CutsceneTrigger : MonoBehaviour
+{
+    private bool triggered = false;
+
+    [SerializeField] private Transform _cutsceneToTrigger;
+
+    private void Start()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<ClaraBehavior>() == null || triggered) return;
+
+        CutsceneManager.ShowCutscene(_cutsceneToTrigger);
+        triggered = true;
+    }
+}
