137c3a9650e1ac1ee21e18e0fd22bb6072d04c12
Mirriu <Irma.Andersson.0465@student.uu.se>
2020-05-20 19:05:00 +0200

    Cutscene trigger now has level-start invoke thing capabilities

--- Clarescent/Assets/Scripts/CutsceneTrigger.cs
+++ Clarescent/Assets/Scripts/CutsceneTrigger.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class CutsceneTrigger : MonoBehaviour
 {
+    private enum InvokeType { Collision, GameStart };
+
+    [SerializeField] private InvokeType _invokeType;
+
     private bool triggered = false;
 
     [SerializeField] private Transform _cutsceneToTrigger;
@@ -12,12 +16,23 @@ public class CutsceneTrigger : MonoBehaviour
     private void Start()
     {
         GetComponent<BoxCollider2D>().isTrigger = true;
+        if(_invokeType == InvokeType.Collision)
+        {
+            TriggerCutScene();
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<ClaraBehavior>() == null || triggered) return;
+        if (collision.GetComponent<ClaraBehavior>() == null || triggered || _invokeType != InvokeType.Collision) return;
 
+        TriggerCutScene();
+    }
+
+    private void TriggerCutScene()
+    {
+        if (triggered) return;
         CutsceneManager.ShowCutscene(_cutsceneToTrigger);
         triggered = true;
     }
