4254dfcca89d094e121ab0a001e620608ae4e66c
Mirriu <Irma.Andersson.0465@student.uu.se>
2020-06-03 19:50:40 +0200

    cutscene in scene zzz

--- Clarescent/Assets/Scripts/CutsceneTrigger.cs
+++ Clarescent/Assets/Scripts/CutsceneTrigger.cs
@@ -16,24 +16,23 @@ public class CutsceneTrigger : MonoBehaviour
     private void Start()
     {
         GetComponent<BoxCollider2D>().isTrigger = true;
-        if(_invokeType == InvokeType.Collision)
+        if(_invokeType == InvokeType.GameStart)
         {
+            print("fucking do the cutscene");
             TriggerCutScene();
         }
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<ClaraBehavior>() == null || triggered || _invokeType != InvokeType.Collision) return;
-
+        if ((collision.GetComponentInParent<ClaraBehavior>() == null && collision.GetComponent<ClaraBehavior>() == null) || _invokeType != InvokeType.Collision) return;
         TriggerCutScene();
     }
 
     private void TriggerCutScene()
     {
-        if (triggered) return;
+        print("Do cutscene");
         CutsceneManager.ShowCutscene(_cutsceneToTrigger);
-        triggered = true;
+        Destroy(gameObject);   
     }
 }
