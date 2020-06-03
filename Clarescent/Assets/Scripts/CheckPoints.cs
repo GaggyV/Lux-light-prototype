b01931010bc530391e49fd9ea11eea74a315df49
GaggyV <60134108+GaggyV@users.noreply.github.com>
2020-06-03 15:19:10 +0200

    Respawner done

---
+++ Clarescent/Assets/Scripts/CheckPoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoints : MonoBehaviour
+{
+    private ReSpawner rs;
+
+    void Start()
+    {
+        rs = GameObject.FindGameObjectWithTag("ReSpawner").GetComponent<ReSpawner>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Clara"))
+        {
+            rs.lastCheckpointPos = transform.position;
+        }
+    }
+}
---
+++ Clarescent/Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private ReSpawner rs;
+
+    void Start()
+    {
+        rs = GameObject.FindGameObjectWithTag("ReSpawner").GetComponent<ReSpawner>();
+        transform.position = rs.lastCheckpointPos;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
